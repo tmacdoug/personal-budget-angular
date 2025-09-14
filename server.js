a320c856456c3fbafac4ea2b720782cfa720b34a
tmacdoug <tmacdoug@uncc.edu>
2025-09-14 15:22:15 -0400

    Loading data from JSON file

--- server.js
+++ server.js
@@ -1,28 +1,12 @@
 // Budget API
-
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const budget = require('.\\budget.json');
 const port = 3000;
 
 app.use(cors());
 
-const budget = {
-    myBudget: [
-        {
-            title: 'Eat out',
-            budget: 25
-        },
-        {
-            title: 'Rent',
-            budget: 275
-        },
-        {
-            title: 'Grocery',
-            budget: 110
-        },
-    ]
-};
 app.use('/', express.static('public'));
 
 app.get('/budget', (req, res) => {
